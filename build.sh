8157c80c170c1e79c12b2a1ebcdbfdfae8babfc2
ananjaser1211 <ananjaser@gmail.com>
2017-12-30 22:49:19 +0400

    Update scripts/Add gitignore

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ export CROSS_COMPILE=/home/elite/android/toolchain/arm-eabi-4.8/bin/arm-eabi-
 # Cleanup
 make clean && make mrproper
 #  Nougat Defcon
-make 
-make 
+make o5lteswa_NN_defconfig
+make exynos3475-on5lte_usa_tmo_00.dtb exynos3475-on5lte_usa_tmo_02.dtb exynos3475-on5lte_usa_tmo_05.dtb
 # Make zImage
 make ARCH=arm -j4
 # Make DT.img
-./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+# ./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
 # Cleaup
-rm -rf $DTS/.*.tmp
-rm -rf $DTS/.*.cmd
-rm -rf $DTS/*.dtb
+# rm -rf $DTS/.*.tmp
+# rm -rf $DTS/.*.cmd
+# rm -rf $DTS/*.dtb
--- dts.sh
+++ dts.sh
@@ -3,8 +3,8 @@ DTS=arch/arm/boot/dts
 RDIR=$(pwd)
 # GCC
 export CROSS_COMPILE=/home/elite/android/toolchain/arm-eabi-4.8/bin/arm-eabi-
-make 
-make 
+make o5lteswa_NN_defconfig
+make exynos3475-on5lte_usa_tmo_00.dtb exynos3475-on5lte_usa_tmo_02.dtb exynos3475-on5lte_usa_tmo_05.dtb
 # Make DT.img
 ./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
 # Cleaup
